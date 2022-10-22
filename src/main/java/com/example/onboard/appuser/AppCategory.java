78b838cbc246b28193bbbc61cd40f5f002f4a1ac
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2022-10-22 04:47:53 +0100

    chore: setup project by securing endpoints

---
+++ src/main/java/com/example/onboard/appuser/AppCategory.java
@@ -0,0 +1,6 @@
+package com.example.onboard.appuser;
+
+public enum AppCategory {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/onboard/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.example.onboard.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
